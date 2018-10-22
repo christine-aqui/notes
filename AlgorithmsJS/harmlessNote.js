72808f59b651356def6b35613397fbc9c8de6adf
christine-aqui <aqui.christine@gmail.com>
2018-10-22 19:50:23 -0400

    done

--- AlgorithmsJS/harmlessNote.js
+++ AlgorithmsJS/harmlessNote.js
@@ -1,20 +1,32 @@
 // harmless ransom note algorithm
 
 const harmlessNote = (noteText, magtext) => {
-    let noteArr = noteText.split(' ');
-    let magArr = magtext.split(' ');
-    // using a hashtable
-    let magObj = {}
-    magArr.forEach( word => {
-        if(!magObj[word]){
-            magObj[word] = 0
-        }
-        magObj[word]++;
-    })
-    console.log(magObj);
-}
+  let noteArr = noteText.split(" ");
+  let magArr = magtext.split(" ");
+  // using a hashtable
+  let magObj = {};
+  magArr.forEach(word => {
+    if (!magObj[word]) {
+      magObj[word] = 0;
+    }
+    magObj[word]++;
+  });
+  //
+  let noteStatus = true;
+  noteArr.forEach(word => {
+    if (magObj[word]) {
+      magObj[word]--;
+      if (magObj[word] < 0) {
+        noteStatus = false;
+      }
+    } else {
+      noteStatus = false;
+    }
+  });
 
+  console.log(magObj);
+  console.log("Note status ", noteStatus);
+};
 
-
-// for testing 
-// harmlessNote('', 'hello world people');
+// for testing
+// harmlessNote("hello people", "hello world people");
